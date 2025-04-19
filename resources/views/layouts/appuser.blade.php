494401fb0bc2366b1432a9f3cd1569224224e8eb
adityafirmansyahasing <benihime311@gmail.com>
2025-04-19 15:35:18 +0700

    tambah fitur order

--- resources/views/layouts/appuser.blade.php
+++ resources/views/layouts/appuser.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{asset('css/cssorder.css')}}">
     
 
     <!-- Style BS -->
---
+++ resources/views/order.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.appuser')
+
+@section('content')
+<div class="c_order-frame">
+    <div class="c_order-frame01">
+      <div class="c_order-text">
+        <p class="c_order-text01">SIBESI : Solusi Air Bersih untuk Kebutuhan Anda!</p>
+      </div>
+      <img
+        src="https://assets.onecompiler.app/42vbxdd3a/43f9kt8bp/c365e850e7b89cf381cfb9fb06806452.png"
+        alt="Banner SIBESI"
+        class="c_order-frame02"
+      />
+    </div>
+
+    <div class="c_order-text02">
+      <p class="c_order-text03">Ukuran dan Jenis Air yang tersedia</p>
+    </div>
+
+    <div class="c_order-frame03">
+      <div class="c_order-frame04" style="display: flex; gap: 50px; flex-wrap: wrap;">
+
+        <!-- mobil tanki air Truk Tangki Air Bersih -->
+        <div class="c_order-card">
+  <div class="c_order-frame-img" style="background-image: url('https://assets.onecompiler.app/42vbxdd3a/43f9kt8bp/da142f6888818d164b5438429709a218.jpg');"></div>
+  <div class="c_order-info">
+    <div class="c_order-info-top">
+      <p class="c_order-text07">Rp. 200.000</p>
+      <div class="c_order-button">
+        <p class="c_order-text05">Pesan</p>
+      </div>
+    </div>
+    <p class="c_order-text09">Truk Tangki Air Bersih</p>
+  </div>
+</div>
+
+
+        <!-- Card 2 Galon Air Bersih -->
+        <div class="c_order-card">
+  <div class="c_order-frame-img" style="background-image: url('https://assets.onecompiler.app/42vbxdd3a/43f9kt8bp/a90200b2ea5b07737034aa0fb7513635.png');"></div>
+  <div class="c_order-info">
+    <div class="c_order-info-top">
+      <p class="c_order-text07">Rp. 10.000</p>
+      <div class="c_order-button">
+        <p class="c_order-text05">Pesan</p>
+      </div>
+    </div>
+    <p class="c_order-text09">Galon Air Bersih</p>
+  </div>
+</div>
+
+        <!-- Card Drum Air Bersih -->
+        <div class="c_order-card">
+  <div class="c_order-frame-img" style="background-image: url('https://assets.onecompiler.app/42vbxdd3a/43f9kt8bp/feef065ca4d032cab37154c6a80889ca.png');"></div>
+  <div class="c_order-info">
+    <div class="c_order-info-top">
+      <p class="c_order-text07">Rp. 50.000</p>
+      <div class="c_order-button">
+        <p class="c_order-text05">Pesan</p>
+      </div>
+    </div>
+    <p class="c_order-text09">Drum Air Bersih</p>
+  </div>
+</div>
+
+        <!-- Card Jerigen Air Bersih -->
+        <div class="c_order-card">
+  <div class="c_order-frame-img" style="background-image: url('https://assets.onecompiler.app/42vbxdd3a/43f9kt8bp/7deaf1ca8626d358adf16830699f77b5.png');"></div>
+  <div class="c_order-info">
+    <div class="c_order-info-top">
+      <p class="c_order-text07">Rp. 30.000</p>
+      <div class="c_order-button">
+        <p class="c_order-text05">Pesan</p>
+      </div>
+    </div>
+    <p class="c_order-text09">Jerigen Air Bersih</p>
+  </div>
+</div>
+
+
+      </div>
+    </div>
+  </div>
+@endsection
